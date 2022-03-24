13246c52e24a9085c5cdb1d23c55641d3d3272ad
John Bell <jbellny@gmail.com>
2022-03-24 11:01:37 -0400

    Initial build

---
+++ src/routes/about.svelte
@@ -0,0 +1,16 @@
+<div class="about">
+    <h2>About Us</h2>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos voluptate explicabo praesentium facilis, laborum debitis, voluptates perspiciatis, eum ab maxime ipsum cumque ad. Excepturi quia laborum natus dolor animi perferendis doloremque nihil possimus, fugiat quasi sint illum? Labore sed consequuntur blanditiis consectetur ea porro aperiam? Iusto unde illum consectetur veritatis recusandae totam excepturi debitis hic, perferendis similique in quidem nemo!</p>
+
+    <a href="/guides">View Guides</a>
+    <a href="/">Home</a>
+
+</div>
+
+<style>
+    .about {
+        text-align: center;
+        display: block;
+        margin: 20px auto
+    }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,17 @@
+
+<div class="index">
+    <h2>Welcome</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error quia modi porro harum totam ipsum expedita, eaque inventore quas quisquam et officiis commodi pariatur omnis, sit eveniet ut iure praesentium? Culpa, quisquam? Vero nisi sapiente quisquam eius pariatur praesentium eos facilis vitae iure asperiores excepturi qui illo recusandae quibusdam iste soluta, quasi, laudantium dolorem error adipisci? Itaque eius inventore aliquid.</p>
+
+    <a href="/guides">View Guides</a>
+    <a href="/about">About</a>
+
+</div>
+
+<style>
+    .index {
+        text-align: center;
+        display: block;
+        margin: 20px auto;
+    }
+</style>

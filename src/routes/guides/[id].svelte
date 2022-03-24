1ec48e5587e52846c582c08d6146c28d411236d4
John Bell <jbellny@gmail.com>
2022-03-24 11:08:34 -0400

    Remove artificial 1sec pause

--- src/routes/guides/[id].svelte
+++ src/routes/guides/[id].svelte
@@ -1,6 +1,5 @@
 <script context="module">
     export async function load({fetch, params}) {
-        await new Promise(resolve => setTimeout(resolve, 1000))
         const id = params.id
         const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
         const guide = await res.json()
